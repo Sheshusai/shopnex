743d6e722dfec53ae5010cbf69aea3a8e47c0e89
Sheshusai <118553988+Sheshusai@users.noreply.github.com>
2024-10-26 14:38:37 +0530

    Update connection.js

--- api/database/connection.js
+++ api/database/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 async function connect() {
-    const db = await mongoose.connect('mongodb+srv://sheshumadhunala1988:W6G8c2fzNl0pNVux@cluster0.5xxde.mongodb.net/');
+    const db = await mongoose.connect('');
 
     console.log('Database Connected !')
     return db;
